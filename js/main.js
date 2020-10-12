3631bf7f794104a8d57884ebe9395dfd9d274309
Andre Heslop <andreheslop@gmail.com>
2020-10-12 13:55:00 -0400

    outcomes update

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ const app = (projects) => {
     // creates a jQuery DOM element based on an individual project object
     const createProjectElement = (project) => {
         const $div = $('<div>').addClass("project")
-        $div.append($('<h2>').text(project.title))
+        $div.append($('<h3>').text(project.title))
         $div.append($('<img>').attr('src', project.image))
         $div.append($('<p>').addClass("description").text(project.description))
         $div.append($('<a>').attr('href', project.url).text("LINK >>"))
